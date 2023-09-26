bbc4fe2e098c9cd372aa6e3ec85d1fbf4b821445
Arjun S <arjun8107@gmail.com>
2023-09-26 15:55:23 +0530

    http 200/404

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,43 @@ use std::io::prelude::*;
 use std::{
     error::Error,
     net::{TcpListener, TcpStream},
+    str,
 };
 
 fn accept_conn(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("connected");
-    const buf: &str = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(buf.as_bytes())?;
+    let mut stream_buffer = [0; 1024];
+    stream.read(&mut stream_buffer).unwrap();
+    let request = str::from_utf8(&stream_buffer).unwrap();
+    let first_line = request
+        .split("\r\n")
+        .collect::<Vec<&str>>()
+        .first()
+        .unwrap()
+        .to_owned();
+    println!("{}", first_line);
+    let first_line_parts = first_line.split_whitespace().collect::<Vec<&str>>();
+    match first_line_parts[1] {
+        "/" => {
+            stream
+                .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                .expect("unable to write to stream");
+        }
+        _ => {
+            stream
+                .write("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                .expect("unable to write to stream");
+        }
+    }
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
+    println!(
+        "Server started at: http://{}",
+        listener.local_addr().unwrap()
+    );
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
