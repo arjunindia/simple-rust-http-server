17038a68e7c7cb085956c21b64e7b8f5b490f83f
Arjun S <arjun8107@gmail.com>
2023-09-26 18:08:35 +0530

    /user-agent

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,47 @@
+mod request;
+use crate::request::Request;
 use std::io::prelude::*;
 use std::{
     error::Error,
     net::{TcpListener, TcpStream},
     str,
 };
-
 fn accept_conn(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("connected");
     let mut stream_buffer = [0; 1024];
     stream.read(&mut stream_buffer).unwrap();
     let request = str::from_utf8(&stream_buffer).unwrap();
-    let first_line = request
-        .split("\r\n")
-        .collect::<Vec<&str>>()
-        .first()
-        .unwrap()
-        .to_owned();
-    print!("{}", first_line);
-    let first_line_parts = first_line.split_whitespace().collect::<Vec<&str>>();
-    match first_line_parts[1] {
-        "/" => {
-            stream
-                .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                .expect("unable to write to stream");
-        }
-        req => {
-            let path_parts = req.split('/').collect::<Vec<&str>>();
-            let (status, body) = if path_parts[1] == "echo" {
-                if path_parts.len() > 1 {
-                    ("200 OK", path_parts[2..].join("/"))
-                } else {
-                    ("200 OK", String::from(""))
-                }
+    let parsed_request = Request::parse(request);
+    print!("{} {}", parsed_request.method, parsed_request.path);
+    if parsed_request.path == "/" {
+        stream
+            .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+            .expect("unable to write to stream");
+    } else {
+        let path_parts = parsed_request.path.split('/').collect::<Vec<&str>>();
+        let (status, body) = if path_parts[1] == "echo" {
+            if path_parts.len() > 1 {
+                ("200 OK", path_parts[2..].join("/"))
             } else {
-                ("404 Not Found", String::from(""))
-            };
-            let resp = format!(
-                "HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-length:{}\r\n\r\n{}",
-                status,
-                body.len(),
-                body
-            );
-            println!(" {}", status);
-            stream
-                .write(resp.as_bytes())
-                .expect("unable to write to stream");
-        }
+                ("200 OK", String::from(""))
+            }
+        } else if path_parts[1] == "user-agent" {
+            ("200 OK", parsed_request.getHeader("User-Agent"))
+        } else {
+            ("404 Not Found", String::from(""))
+        };
+        let resp = format!(
+            "HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-length:{}\r\n\r\n{}",
+            status,
+            body.len(),
+            body
+        );
+        println!(" {}", status);
+        stream
+            .write(resp.as_bytes())
+            .expect("unable to write to stream");
     }
+
     Ok(())
 }
 
---
+++ src/request.rs
@@ -0,0 +1,37 @@
+pub struct Request {
+    pub method: String,
+    pub path: String,
+    pub headers: Vec<Header>,
+}
+type Header = (String, String);
+impl Request {
+    pub fn parse(string: &str) -> Self {
+        let mut request = Self {
+            method: String::new(),
+            path: String::new(),
+            headers: Vec::new(),
+        };
+        let req_lines = string.split("\r\n").collect::<Vec<&str>>();
+        let first_line = req_lines.first().unwrap();
+        let first_line_parts = first_line.split_whitespace().collect::<Vec<&str>>();
+        request.method = String::from(first_line_parts[0]);
+        request.path = String::from(first_line_parts[1]);
+
+        for (_, line) in req_lines[1..].iter().enumerate() {
+            let header = match line.split_once(":") {
+                Some(val) => (String::from(val.0), String::from(val.1)),
+                None => ("".to_owned(), "".to_owned()),
+            };
+            request.headers.push(header);
+        }
+        request
+    }
+    pub fn getHeader(&self, name: &str) -> String {
+        for header in self.headers.iter() {
+            if header.0 == name.to_owned() {
+                return header.1.clone();
+            }
+        }
+        return String::new();
+    }
+}
