2a00d2039465ed312442524d1748308f964e833e
Arjun S <arjun8107@gmail.com>
2023-09-26 15:34:07 +0530

    http res

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::{
+    error::Error,
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
-    println!("Logs from your program will appear here!");
+fn accept_conn(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    println!("connected");
+    const buf: &str = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(buf.as_bytes())?;
+    Ok(())
+}
 
+fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                accept_conn(_stream)?;
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("error: {}", e),
         }
     }
+    Ok(())
 }
