ffe08df3ab7a1e8e6bdf1b62ce9fde3f3bd95080
Arjun S <arjun8107@gmail.com>
2023-09-26 20:58:09 +0530

    fixes + file support?

--- src/request.rs
+++ src/request.rs
@@ -28,7 +28,7 @@ impl Request {
             };
             request.headers.push(header);
         }
-        request.body = string.split("\r\n\r\n").collect_vec()[1].into();
+        request.body = string.split("\r\n\r\n").collect::<Vec<&str>>()[1].to_string();
 
         request
     }
