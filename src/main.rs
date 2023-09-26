216333f550850c4b91beec310529bd29bc2fb3d8
Arjun S <arjun8107@gmail.com>
2023-09-26 21:50:41 +0530

    fixes + file support?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod request;
 use crate::request::Request;
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::prelude::*;
 use std::{env, thread};
 use std::{
@@ -45,9 +45,11 @@ fn accept_conn(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
                     ),
                 }
             } else {
-                match fs::read_to_string(format!("{dir}{filename}")) {
-                    Ok(contents) => {
-                        let trimmed_contents = contents.trim();
+                let mut file = File::open(format!("{dir}{filename}"))?;
+                let mut contents = String::new();
+                match file.read_to_string(&mut contents) {
+                    Ok(_) => {
+                        let trimmed_contents = contents.to_string();
                         println!("{contents},{}", trimmed_contents.len());
                         (
                             "200 Ok",
